7bb755757ffdfbcf41fc9d2d18c6f4c832e12edb
Olena Sytnyk <olenasytnyk@Olenas-MacBook-Air.local>
2023-03-11 18:21:49 +0200

    fix syntax errors

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,9 @@
 import random
 
-text_dishes = input("--Кафе 'У Монті'--\nЗамовляйте страви\n") dishes = text_dishes.split(",") 
-total_sum = 0 for dish in dishes:
+text_dishes = input("--Кафе 'У Монті'--\nЗамовляйте страви\n") 
+dishes = text_dishes.split(",") 
+total_sum = 0 
+for dish in dishes:
     dish_price = round(random.random() * 100, 2)
     total_sum += dish_price
     print(dish.strip() + "......." + str(dish_price) + " грн")
