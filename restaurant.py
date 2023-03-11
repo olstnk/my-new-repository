081f45110615ff16f8bc6779242c323d2aa01b53
Olena Sytnyk <olenasytnyk@Olenas-MacBook-Air.local>
2023-03-11 18:11:55 +0200

    add restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,9 @@
+import random
+
+text_dishes = input("--Кафе 'У Монті'--\nЗамовляйте страви\n") dishes = text_dishes.split(",") 
+total_sum = 0 for dish in dishes:
+    dish_price = round(random.random() * 100, 2)
+    total_sum += dish_price
+    print(dish.strip() + "......." + str(dish_price) + " грн")
+print(".................")
+print("Всього........" + str(round(total_sum, 2)) + " грн") 
